367c78172d70b02036c669d9dd17af7004bcc5a6
Peter Vallet <pvalle6@lsu.edu>
2024-10-22 21:35:54 -0500

    fixed integration error with regard to changing input

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -14,7 +14,9 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # , NavigationToolbar2Tk
 import webbrowser
+import random
 
+# TODO: Multithreading
 # Setting up dataframes for simulation
 simulation_data = pd.DataFrame(columns=['time', 'flow101', 'flow100', 'cooling_temp',
                                         'reactor_temp', 'feed_temp',
@@ -22,16 +24,6 @@ simulation_data = pd.DataFrame(columns=['time', 'flow101', 'flow100', 'cooling_t
                                         'feed_composition', 'jacket_temp_out'])
 
 history_df = pd.DataFrame(columns=['time', 'bulk_a_comp', 'reactor_temp', 'jacket_temp_out'])
-
-# Feed Specifications
-feed_comp_a = 0  # mol / m^3
-feed_comp_b = 0  # mol / m^3
-feed_flow = 0.1  # m^3 / s
-feed_temp = 300  # K
-
-# Jacket Specifications
-jacket_temp_in = 280  # K
-jack_feed_flow = 0.1  # m^3 / s
 
 # jacket_volume = 0.1 # m^3
 
@@ -89,7 +81,7 @@ def print_simulation_data():
     This function prints the simulation data to the console.
     :return:
     """
-    print(simulation_data)
+    print(simulation_data.to_string())
 
 
 def show_menu(event):
@@ -138,6 +130,15 @@ def open_linkedin():
 def open_pse():
     webbrowser.open('https://pse.che.lsu.edu/')
 
+# Feed Specifications
+feed_comp_a = 2.353  # kmol / m^3
+feed_flow = 1  # m^3 / s
+feed_temp = 362  # K
+
+# Jacket Specifications
+jacket_temp_in = 150  # K
+jack_feed_flow = 0.1  # m^3 / s
+
 
 def run_simulation():
     """
@@ -148,62 +149,48 @@ def run_simulation():
     global history_df
     global simulation_data
 
-    start_time = time.time()
-    time_now = 0
     # history_df = pd.DataFrame(columns=['time', 'bulk_a_comp', 'reactor_temp', 'jacket_temp_out'])
 
-    bulk_comp_a = 0
-    reactor_temp = 300
-    temperate_jacket_out = 300
+    bulk_comp_a = 2.353  # kmol / m^3
+    reactor_temp = 362.4  # K
+    temperate_jacket_out = 345.69  # K
 
-    sol = scipy.integrate.solve_ivp(system_of_equations, [0, 1],
+    sol = scipy.integrate.solve_ivp(system_of_equations, [0, 0.1],
                                     [bulk_comp_a, reactor_temp, temperate_jacket_out],
-                                    args=(feed_flow, jack_feed_flow))
+                                    args=(feed_flow, feed_comp_a, feed_temp, jack_feed_flow), first_step=0.0001)
     history_df = pd.DataFrame(
         {'time': 0, 'bulk_a_comp': sol.y[0], "reactor_temp": sol.y[1], "jacket_temp_out": sol.y[2]})
 
-    previous_time_step = 1
+    last_time = 0.1
 
-    for i in range(0, 1000000):
-        sol = scipy.integrate.solve_ivp(system_of_equations, [0, 1],
+    for i in range(0, 10000):
+
+        sol = scipy.integrate.solve_ivp(system_of_equations, [last_time, last_time + 0.1],
                                         [history_df['bulk_a_comp'].iloc[-1], history_df['reactor_temp'].iloc[-1],
-                                         history_df['jacket_temp_out'].iloc[-1]], args=(feed_flow, jack_feed_flow))
+                                         history_df['jacket_temp_out'].iloc[-1]],
+                                        args=(feed_flow, feed_comp_a, feed_temp, jack_feed_flow), method="BDF")
         history_df = pd.concat([history_df, pd.DataFrame(
-            {"time": time_now, 'bulk_a_comp': sol.y[0], "reactor_temp": sol.y[1], "jacket_temp_out": sol.y[2]})])
+            {"time": last_time + 0.1, 'bulk_a_comp': sol.y[0], "reactor_temp": sol.y[1], "jacket_temp_out": sol.y[2]})])
+
+        last_time = last_time + 0.1
+
+        if i % 100 == 0:
+            print(i)
+
+            print(history_df)
 
         simulation_data = pd.concat([simulation_data, pd.DataFrame(
-            {"time": time_now, "flow101": flow101_indicator.get(),
+            {"time": last_time, "flow101": flow101_indicator.get(),
              "flow100": flow100_indicator.get(), "cooling_temp": cw_temp_indicator.get(),
              "reactor_temp": reactor_temp_indicator.get(), "feed_temp": feed_temp_indicator.get(),
              "exit_temp": exit_temp_indicator.get(), "level": level_indicator.get(),
              "exit_composition": exit_comp_indicator.get(), "feed_composition": feed_comp_indicator.get(),
              "jacket_temp_out": sol.y[2][-1]}, index=[0])])
 
-        previous_time_step += 1
-        # print(history_df.tail(1))
-
         reactor_temp_indicator.set(str(round(sol.y[1][-1], 2)) + " K")
         cool_temp_out_indicator.set(str(round(sol.y[2][-1], 2)) + " K")
         exit_comp_indicator.set(str(round(sol.y[0][-1], 2)) + " mol / m^3")
         window.update()
-        time_now = time.time() - start_time
-        time.sleep(0.001)
-        if stopped:
-            break
-
-# def update_time(start_time):
-#     """
-#     This function updates the time in the GUI.
-#     :return:
-#     """
-#     elapsed_time = str(int(time.time() - start_time))  # Get the number of seconds since the start
-#     clock = "Time Elapsed: " + elapsed_time + " seconds"
-#     time_var.set(clock)  # Update the time_var with the new time
-#     # print(elapsed_time)
-#     # window.after(1000, update_time)  # Schedule the function to be called again after 1 second
-#
-#     return elapsed_time
-
 
 def open_f101_valve():
     """
@@ -379,13 +366,13 @@ def open_sensor_graph(sensor_name):
     print(f"Opening {sensor_name} Sensor Window")
     sensor_window = tk.Toplevel(window)
     sensor_window.title(f"{sensor_name} Sensor")
-    sensor_window.geometry("450x450")
+    sensor_window.geometry("500x500")
 
-    sensor_graph = Figure(figsize=(5, 4), dpi=100)
+    sensor_graph = Figure(figsize=(6, 5), dpi=100)
     sensor_plot = sensor_graph.add_subplot(111)
     sensor_plot.plot(data["time"], data[sensor_name])
     sensor_plot.set_xlabel("Time (s)")
-    sensor_plot.set_ylabel(f"{sensor_name} (units)")
+    # sensor_plot.set_ylabel(f"{sensor_name} (units)")
 
     sensor_canvas = FigureCanvasTkAgg(sensor_graph, sensor_window)
     sensor_canvas.get_tk_widget().pack()
@@ -547,32 +534,32 @@ exit_c_button.config(command=lambda: on_exit_comp_sensor(1))
 exit_c_button.place(x=680, y=510)
 
 flow100_indicator = tk.StringVar()
-flow100_indicator.set("0")
+flow100_indicator.set("0.1")
 flow100_label = tk.Label(frame, textvariable=flow100_indicator, font="Arial 12 bold")
 flow100_label.place(x=110, y=300)
 
 flow101_indicator = tk.StringVar()
-flow101_indicator.set("0")
+flow101_indicator.set("0.1")
 flow101_label = tk.Label(frame, textvariable=flow101_indicator, font="Arial 12 bold")
 flow101_label.place(x=590, y=130)
 
 cw_temp_indicator = tk.StringVar()
-cw_temp_indicator.set("0")
+cw_temp_indicator.set("280")
 cooling_temp_label = tk.Label(frame, textvariable=cw_temp_indicator, font="Arial 12 bold")
 cooling_temp_label.place(x=180, y=210)
 
 reactor_temp_indicator = tk.StringVar()
-reactor_temp_indicator.set("0")
+reactor_temp_indicator.set("300")
 reactor_temp_label = tk.Label(frame, textvariable=reactor_temp_indicator, font="Arial 12 bold")
 reactor_temp_label.place(x=70, y=487)
 
 feed_temp_indicator = tk.StringVar()
-feed_temp_indicator.set("0")
+feed_temp_indicator.set("300")
 feed_temp_label = tk.Label(frame, textvariable=feed_temp_indicator, font="Arial 12 bold")
 feed_temp_label.place(x=460, y=20)
 
 exit_temp_indicator = tk.StringVar()
-exit_temp_indicator.set("0")
+exit_temp_indicator.set("299")
 exit_temp_label = tk.Label(frame, textvariable=exit_temp_indicator, font="Arial 12 bold")
 exit_temp_label.place(x=570, y=550)
 
@@ -582,37 +569,37 @@ level_label = tk.Label(frame, textvariable=level_indicator, font="Arial 12 bold"
 level_label.place(x=450, y=500)
 
 exit_comp_indicator = tk.StringVar()
-exit_comp_indicator.set("0")
+exit_comp_indicator.set("0.45")
 exit_composition_label = tk.Label(frame, textvariable=exit_comp_indicator, font="Arial 12 bold")
 exit_composition_label.place(x=670, y=470)
 
 feed_comp_indicator = tk.StringVar()
-feed_comp_indicator.set("0")
+feed_comp_indicator.set("0.1")
 feed_composition_label = tk.Label(window, textvariable=feed_comp_indicator, font="Arial 12 bold")
 feed_composition_label.place(x=450, y=270)
 
 cool_temp_out_indicator = tk.StringVar()
-cool_temp_out_indicator.set("0")
+cool_temp_out_indicator.set("295")
 cool_temp_out_label = tk.Label(frame, textvariable=cool_temp_out_indicator, font="Arial 12 bold")
 cool_temp_out_label.place(x=550, y=270)
 
 
-def system_of_equations(t, y, feed_flow, jack_feed_flow):
+def system_of_equations(t, y, feed_flow, feed_a_comp, feed_temp, jack_feed_flow):
     # y[0] = bulk_a_comp
     # y[1] = reactor_temp
     # y[2] = temperature_jacket_out
 
     # Constants
-    reactor_volume = 0.2  # m^3
-    k1 = 0.1  # rate constant for reaction 1
+    reactor_volume = 10  # m^3
+    k1 = 0.85  # rate constant for reaction 1
     jacket_volume = 0.1  # m^3
     heat_transfer_coefficient = 0.1  # W / m^2 K
     area_of_transfer = 0.1  # m^2
 
     # constants that later should be dynamically changed
     feed_flow = feed_flow  # m^3 / s
-    feed_a_comp = 0.5  # mol / m^3
-    feed_temp = 300  # K
+    feed_a_comp = feed_a_comp  # mol / m^3
+    feed_temp = feed_temp  # K
 
     energy_activation = 0.1  # J / mol
     R = 8.314  # J / mol K
@@ -624,11 +611,11 @@ def system_of_equations(t, y, feed_flow, jack_feed_flow):
     jacket_flow = jack_feed_flow  # m^3 / s
 
     # Differential Equations
-    dCadt = ((feed_flow / reactor_volume * feed_a_comp) - (feed_flow / reactor_volume * y[0])
+    dCadt = (((feed_flow / reactor_volume) * feed_a_comp) - ((feed_flow / reactor_volume) * y[0])
              - k1 * np.exp(-energy_activation / (R * y[1])) * y[0])
 
     dTdt = ((((feed_flow / reactor_volume) * (feed_temp - y[1])) +
-            ((-enthalpy_change / (density * heat_capacity)) * k1 * np.exp(-energy_activation / (R * y[1])) * y[0]))
+            ((enthalpy_change / (density * heat_capacity)) * k1 * np.exp(-energy_activation / (R * y[1])) * y[0]))
             - ((heat_transfer_coefficient * area_of_transfer / (density * heat_capacity * reactor_volume)) *
                (y[1] - y[2])))
 
@@ -636,6 +623,7 @@ def system_of_equations(t, y, feed_flow, jack_feed_flow):
              (heat_transfer_coefficient * area_of_transfer / (density * heat_capacity * jacket_volume)) * (y[1] - y[2]))
 
     return [dCadt, dTdt, dTjdt]
+
 
 
 # Setting up the initial values for the simulation
